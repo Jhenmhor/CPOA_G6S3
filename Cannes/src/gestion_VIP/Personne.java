860a3f796e3c5614529fed173171e977dd2475fc
Jhenmhor <jhenmhor@gmail.com>
2018-06-08 10:47:10 +0200

    upd

--- Cannes/src/gestion_VIP/Personne.java
+++ Cannes/src/gestion_VIP/Personne.java
@@ -14,15 +14,47 @@ public class Personne {
     private final int id;
     private String nom;
     private String prenom;
-
+    private String mdp;
+    private String type;
+    
+    
     public Personne(int id) {
         this.id = id;
     }
 
-    public Personne(int id, String nom, String prenom) {
+    public Personne(int id, String nom, String prenom, String type, String mdp) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
+        this.mdp = mdp;
+        this.type = type;
     }
-
+    
+//<editor-fold defaultstate="collapsed" desc="Getter & Setter">
+    
+    public String getNom() {
+        return nom;
+    }
+    
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    public String getPrenom() {
+        return prenom;
+    }
+    
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+    public String getMdp() {
+        return mdp;
+    }
+    
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+//</editor-fold>
+    
 }
